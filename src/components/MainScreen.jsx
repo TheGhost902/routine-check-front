3c12584ee5cc98e4fc55e394e034276c04b1328b
TheGhost902 <emilc902@gmail.com>
2020-03-14 17:21:45 +0300

    minor fixes

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,41 +1,15 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { routinesFetched, deleteRoutine } from '../redux/actionCreators'
+import { routinesFetched } from '../redux/actionCreators'
 import { Link } from 'react-router-dom'
 import { updateRoutinesData } from '../utils'
-import toast from './toast'
 
 const mapDispatchToProps = {
-    routinesFetched,
-    deleteRoutine
+    routinesFetched
 }
 const mapStateToProps = ({ routines }) => ({routines})
 
-function MainScreen({ routinesFetched, deleteRoutine, routines }) {
-    async function deleteSomeRoutine(id) {
-        try {
-            const response = await fetch('/routines', {
-                method: 'DELETE',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify({routineId: id})
-            })
-            const parsedResponse = await response.json()
-
-            if (parsedResponse.message) {
-                toast(parsedResponse.message.text, parsedResponse.message.type)
-            }
-
-            if (parsedResponse.routineId) {
-                deleteRoutine(parsedResponse.routineId)
-            }
-
-        } catch (err) {
-            toast('Some Network problems...', 'error')
-        } 
-    }
-
+function MainScreen({ routinesFetched, routines }) {
     useEffect(() => {
         updateRoutinesData(routinesFetched)
     }, [])
@@ -56,9 +30,6 @@ function MainScreen({ routinesFetched, deleteRoutine, routines }) {
                     {routines.map(routine =>
                         <li className="main-screen__list-element" key={routine._id}>
                             <Link to={`/routine/${routine._id}`}>{routine.title}</Link>
-                            
-                            {/* <button onClick={() => deleteSomeRoutine(routine._id)}>delete</button> */}
-                            
                         </li>
                     )}
                 </ul>
